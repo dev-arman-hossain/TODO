cc60bd3efb8f3e5de5cb77964de4d445efb4ceb8
Arman Hossain <web.devarman@gmail.com>
2025-12-04 04:00:43 +0600

    add test user

--- src/server.ts
+++ src/server.ts
@@ -48,8 +48,24 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, bro! whats up?");
 });
 
-app.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+      [name, email]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: result.rows[0],
+    });
+    // console.log(result.rows[0]);
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
   res.status(201).json({
     success: true,
     message: "Api is working fine",
