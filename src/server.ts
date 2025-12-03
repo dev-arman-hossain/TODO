d897052e7ae113846296606f01758bd6ddcddb88
Arman Hossain <web.devarman@gmail.com>
2025-12-04 05:30:03 +0600

    users CRUD

--- src/server.ts
+++ src/server.ts
@@ -48,6 +48,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, bro! whats up?");
 });
 
+//users CRUD
 app.post("/users", async (req: Request, res: Response) => {
   const { name, email } = req.body;
   try {
@@ -66,10 +67,67 @@ app.post("/users", async (req: Request, res: Response) => {
     console.error(err);
     res.status(500).json({ error: "Internal Server Error" });
   }
-  res.status(201).json({
-    success: true,
-    message: "Api is working fine",
-  });
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.status(200).json(result.rows);
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", [
+      req.params.id,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    } else {
+      res.status(200).json(result.rows[0]);
+    }
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      `UPDATE users SET name = $1, email = $2, updated_at = NOW() WHERE id = $3 RETURNING *`,
+      [name, email, req.params.id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    } else {
+      res.status(200).json(result.rows[0]);
+    }
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      `DELETE FROM users WHERE id = $1 RETURNING *`,
+      [req.params.id]
+    );
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "User not found" });
+    } else {
+      res.status(200).json(result.rows[0]);
+    }
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => {
